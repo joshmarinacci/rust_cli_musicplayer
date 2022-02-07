b0fb6ebf47c31b51a8e5cc85c1a1318d679db3e6
Josh Marinacci <joshua@marinacci.org>
2022-02-06 17:51:54 -0800

    enable switching audio tracks

--- src/audio.rs
+++ src/audio.rs
@@ -2,11 +2,12 @@ use walkdir::{DirEntry, WalkDir};
 use std::fs::File;
 use std::path::PathBuf;
 use std::sync::mpsc::Receiver;
-use symphonia::default::get_codecs;
+use symphonia::default::{get_codecs, get_probe};
 use std::thread;
 use std::time::Duration;
 use symphonia::core::formats::FormatReader;
 use symphonia::core::codecs::Decoder;
+use symphonia::core::probe::ProbeResult;
 use crate::{AudioOutput, CODEC_TYPE_NULL, DecoderOptions, Error, FormatOptions, get_or, Hint, MediaSourceStream, MetadataOptions, output, ProbedMetadata, StandardTagKey, TrackData};
 
 pub enum AudioCommand {
@@ -36,7 +37,7 @@ pub fn scan_for_tracks(music_dir: &str) -> Vec<TrackData> {
             hint.with_extension("mp3");
             let meta_opts: MetadataOptions = Default::default();
             let fmt_opts: FormatOptions = Default::default();
-            if let Ok(probed) = &mut symphonia::default::get_probe()
+            if let Ok(probed) = &mut get_probe()
                 .format(&hint, mss, &fmt_opts, &meta_opts) {
                 tracks.push(process_metadata(&mut probed.metadata, ent.path().to_path_buf()));
             }
@@ -78,7 +79,98 @@ fn process_metadata(probed: &mut ProbedMetadata, buf: PathBuf) -> TrackData {
     return track;
 }
 
+struct AudioContext {
+    decoder: Box<dyn Decoder>,
+    track_id: u32,
+    probe_result: ProbeResult,
+}
 pub fn play_audio(track: &TrackData, audio_output: &mut Option<Box<dyn AudioOutput>>, rec: Receiver<AudioCommand>) -> crate::Result<()>{
+    let mut ctx = open_audio_track(track);
+
+    println!("got the track id {}",ctx.track_id);
+    let mut running = true;
+    loop {
+        if let Ok(msg) = rec.try_recv() {
+            // println!("got the play pause over here");
+            match msg {
+                AudioCommand::Play(track) => {
+                    println!("got a new track to play {}", get_or(&track.title,"unknown title"));
+                    ctx = open_audio_track(&track);
+                }
+                AudioCommand::TogglePlayPause => running = !running,
+                AudioCommand::Quit => {
+                    running = false;
+                    break;
+                }
+            }
+        }
+        if !running {
+            //sleep for 10th of a second then continue;
+            thread::sleep(Duration::from_millis(100));
+            continue;
+        }
+        let packet = match ctx.probe_result.format.next_packet() {
+            Ok(packet) => packet,
+            Err(Error::ResetRequired) => {
+                println!("reset required");
+                unimplemented!()
+            }
+            Err(err) => {
+                println!("error . end of stream?");
+                break;
+            }
+        };
+        if packet.track_id() != ctx.track_id {
+            println!("continuing");
+            continue;
+        }
+        match ctx.decoder.decode(&packet) {
+            Ok(decoded) => {
+                // Consume the decoded audio samples (see below).
+                // println!("got some samples {}", decoded.frames());
+                if audio_output.is_none() {
+                    // println!("trying to open a device");
+                    // Get the audio buffer specification. This is a description of the decoded
+                    // audio buffer's sample format and sample rate.
+                    let spec = *decoded.spec();
+                    println!("spec is {:?}",spec);
+
+                    // Get the capacity of the decoded buffer. Note that this is capacity, not
+                    // length! The capacity of the decoded buffer is constant for the life of the
+                    // decoder, but the length is not.
+                    let duration = decoded.capacity() as u64;
+                    println!("duraction is {}",duration);
+
+                    // Try to open the audio output.
+                    audio_output.replace(output::try_open(spec, duration).unwrap());
+
+                } else {
+                    // println!("still open");
+                }
+                if let Some(audio_output) = audio_output {
+                    audio_output.write(decoded).unwrap()
+                }
+            }
+            Err(Error::IoError(_)) => {
+                println!("io error");
+                // The packet failed to decode due to an IO error, skip the packet.
+                continue;
+            }
+            Err(Error::DecodeError(_)) => {
+                println!("decode error");
+                // The packet failed to decode due to invalid data, skip the packet.
+                continue;
+            }
+            Err(err) => {
+                // An unrecoverable error occured, halt decoding.
+                println!("{}", err);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn open_audio_track(track: &TrackData) -> AudioContext {
     let src = File::open(&track.path).expect("couldnt open the file");
     let mss = MediaSourceStream::new(Box::new(src), Default::default());
 
@@ -87,109 +179,22 @@ pub fn play_audio(track: &TrackData, audio_output: &mut Option<Box<dyn AudioOutp
     hint.with_extension("mp3");
     let meta_opts: MetadataOptions = Default::default();
     let fmt_opts: FormatOptions = Default::default();
-    if let Ok(probed) = &mut symphonia::default::get_probe().format(&hint, mss, &fmt_opts, &meta_opts) {
-        // let mut format = &probed.format;
+    let probe_result = get_probe().format(&hint, mss, &fmt_opts, &meta_opts).unwrap();
 
         // Find the first audio track with a known (decodeable) codec.
-        let track = probed.format.tracks()
-            .iter()
-            .find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
-            .expect("no supported audio tracks");
+    let track = probe_result.format.tracks()
+        .iter()
+        .find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
+        .expect("no supported audio tracks");
+    let track_id = track.id;
 
         // Use the default options for the decoder.
-        let dec_opts: DecoderOptions = Default::default();
-
+    let dec_opts: DecoderOptions = Default::default();
         // Create a decoder for the track.
-        let mut decoder = get_codecs().make(&track.codec_params, &dec_opts)?;
-
-        // let tb = track.codec_params.time_base;
-        // let dur = track.codec_params.n_frames.map(|frames|track.codec_params.start_ts+frames);
-
-        // Store the track identifier, it will be used to filter packets.
-        let track_id = track.id;
-
-        println!("got the track id {}",track_id);
-        let mut running = true;
-        loop {
-            if let Ok(msg) = rec.try_recv() {
-                // println!("got the play pause over here");
-                match msg {
-                    AudioCommand::Play(track) => {
-                        println!("got a new track to play {}", get_or(&track.title,"unknown title"));
-                    }
-                    AudioCommand::TogglePlayPause => running = !running,
-                    AudioCommand::Quit => {
-                        running = false;
-                        break;
-                    }
-                }
-            }
-            if !running {
-                //sleep for 10th of a second then continue;
-                thread::sleep(Duration::from_millis(100));
-                continue;
-            }
-            let packet = match probed.format.next_packet() {
-                Ok(packet) => packet,
-                Err(Error::ResetRequired) => {
-                    println!("reset required");
-                    unimplemented!()
-                }
-                Err(err) => {
-                    println!("error . end of stream?");
-                    break;
-                }
-            };
-
-            if packet.track_id() != track_id {
-                println!("continuing");
-                continue;
-            }
-            match decoder.decode(&packet) {
-                Ok(decoded) => {
-                    // Consume the decoded audio samples (see below).
-                    // println!("got some samples {}", decoded.frames());
-                    if audio_output.is_none() {
-                        // println!("trying to open a device");
-                        // Get the audio buffer specification. This is a description of the decoded
-                        // audio buffer's sample format and sample rate.
-                        let spec = *decoded.spec();
-                        println!("spec is {:?}",spec);
-
-                        // Get the capacity of the decoded buffer. Note that this is capacity, not
-                        // length! The capacity of the decoded buffer is constant for the life of the
-                        // decoder, but the length is not.
-                        let duration = decoded.capacity() as u64;
-                        println!("duraction is {}",duration);
-
-                        // Try to open the audio output.
-                        audio_output.replace(output::try_open(spec, duration).unwrap());
-
-                    } else {
-                        // println!("still open");
-                    }
-                    if let Some(audio_output) = audio_output {
-                        audio_output.write(decoded).unwrap()
-                    }
-                }
-                Err(Error::IoError(_)) => {
-                    println!("io error");
-                    // The packet failed to decode due to an IO error, skip the packet.
-                    continue;
-                }
-                Err(Error::DecodeError(_)) => {
-                    println!("decode error");
-                    // The packet failed to decode due to invalid data, skip the packet.
-                    continue;
-                }
-                Err(err) => {
-                    // An unrecoverable error occured, halt decoding.
-                    println!("{}", err);
-                }
-            }
-
-        }
+    let decoder = get_codecs().make(&track.codec_params, &dec_opts).unwrap();
+    AudioContext {
+        decoder,
+        probe_result,
+        track_id:track_id,
     }
-    Ok(())
-
 }
